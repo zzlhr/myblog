a761312db8373147d60af2f41b043d7f5722151f
lhr <2388399752@qq.com>
2017-09-01 15:47:49 +0800

    管理员操作

---
+++ src/main/java/com/zzlhr/service/AuthorityService.java
@@ -0,0 +1,7 @@
+package com.zzlhr.service;
+
+/**
+ * Created by 刘浩然 on 2017/9/1.
+ */
+public interface AuthorityService {
+}
---
+++ src/main/java/com/zzlhr/service/impl/AuthorityServiceDao.java
@@ -0,0 +1,10 @@
+package com.zzlhr.service.impl;
+
+import com.zzlhr.service.AuthorityService;
+
+/**
+ * Created by 刘浩然 on 2017/9/1.
+ */
+public class AuthorityServiceDao implements AuthorityService {
+
+}
