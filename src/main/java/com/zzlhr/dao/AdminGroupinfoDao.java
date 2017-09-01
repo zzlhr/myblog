bd25bfd53b0c3ac2c1e06187aeb88ea8eedf60d6
lhr <2388399752@qq.com>
2017-09-01 15:44:56 +0800

    权限数据库操作

---
+++ src/main/java/com/zzlhr/dao/AdminGroupinfoDao.java
@@ -0,0 +1,10 @@
+package com.zzlhr.dao;
+
+import com.zzlhr.entity.AdminGroupinfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by 刘浩然 on 2017/9/1.
+ */
+public interface AdminGroupinfoDao extends JpaRepository<AdminGroupinfo, Integer>{
+}
---
+++ src/main/java/com/zzlhr/dao/MenuDoDao.java
@@ -0,0 +1,10 @@
+package com.zzlhr.dao;
+
+import com.zzlhr.entity.MenuDo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by 刘浩然 on 2017/9/1.
+ */
+public interface MenuDoDao extends JpaRepository<MenuDo, Integer>{
+}
