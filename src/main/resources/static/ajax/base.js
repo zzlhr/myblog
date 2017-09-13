25a100ff0bd762e56f2457d167ffb7cad40d37cf
zzlhr <2388399752@qq.com>
2017-09-13 10:22:32 +0800

    基本js和样式

--- src/main/resources/static/ajax/base.js
+++ src/main/resources/static/ajax/base.js
@@ -24,17 +24,13 @@ function baseurl() {
 }
 
 
-function getAbout(callback) {
-    ajax(baseurl()+"/about.json", 0, '',function (data) {
-        callback(data);
-    })
-}
-
-
+/**
+ * 设置关于
+ */
 function setAbout() {
     var about;
     if(sessionStorage.getItem("about") == null){
-        getAbout(function (data) {
+        ajax(baseurl()+"/about.json", 0, '',function (data) {
             about = data;
             var model = "<p>姓名："+about.aboutName+"</p>" +
                 "<p>性别："+about.aboutSex+"</p>" +
@@ -52,7 +48,26 @@ function setAbout() {
             "<p>"+about.aboutOther+"</p>";
         $('.blog-about').html(model);
     }
+}
 
 
+function setNewArticle() {
 
+    var model = '<li class="list-group-item clearfix" style="padding: 5px;">' +
+        '<p class="blog-li-text15" style="margin: 0px;"><a class="pull-left" href="(href)">(title)</a></p>' +
+        '<span class="pull-right">(time)</span>' +
+        '</li>'
+
+    ajax("/articles.json",0,'',function (data) {
+        var mhtml = '';
+
+        for (var i = 0; i < data.length; i++){
+            mhtml += model.replace('(href)', baseurl() + "/article.html?id=" + data[i].id)
+                .replace('(title)', data[i].title)
+                .replace('(time)', data[i].time);
+        }
+
+        $('.blog-commend-left').html(mhtml);
+    })
+    
 }
