8898391788cb2d6124eb007021ba169fcca91441
zzlhr <2388399752@qq.com>
2017-10-10 11:34:47 +0800

    改名....

--- src/main/java/com/zzlhr/dao/AuthModelDao.java
+++ src/main/java/com/zzlhr/dao/AuthModelDao.java
@@ -1,10 +1,12 @@
 package com.zzlhr.dao;
 
 
-import com.zzlhr.entity.AuthOperate;
+import com.zzlhr.entity.AuthModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AutoOperateDao extends JpaRepository<AuthOperate, Integer> {
+public interface AuthModelDao extends JpaRepository<AuthModel, Integer>{
+
+
 }
--- src/main/java/com/zzlhr/dao/AuthOperateDao.java
+++ src/main/java/com/zzlhr/dao/AuthOperateDao.java
@@ -1,12 +1,10 @@
 package com.zzlhr.dao;
 
 
-import com.zzlhr.entity.AuthModel;
+import com.zzlhr.entity.AuthOperate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AutoModelDao extends JpaRepository<AuthModel, Integer>{
-
-
+public interface AuthOperateDao extends JpaRepository<AuthOperate, Integer> {
 }
